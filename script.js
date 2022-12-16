8efd78bd6f5ffc94f1225e1d3e6455c0585c4093
Jacob Roth <jakeroth0@gmail.com>
2022-12-16 16:38:47 -0500

    cors prob

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ var fiveDayForecastCardnput = document.querySelector('.fiveDayForecastCard');
 // var city = userInput.value.trim();
 var apiKey = '&appid=890919f21758bd5b25e56343a417bb71';
 var apiStart = 'api.openweathermap.org/data/2.5/forecast?lat=';
-var lat =''
-// var lonKey = '&lon=';
-// var lon = ''
+// var lat ='37.5385087'
+var lonKey = '&lon=';
+// var lon = '-77.43428'
 var apiCoorStart = 'http://api.openweathermap.org/geo/1.0/direct?q=';
 var coorLimit = '&limit=';
 var limit = '';
@@ -21,12 +21,13 @@ var formSubmitHandler = function (event) {
     var city = userInput.value.trim();
     event.preventDefault();
     console.log(city);
-    var getWeather = function() {
+    var getCoor = function() {
         apiCoor = apiCoorStart + city + coorLimit + 1 + apiKey;
         fetch(apiCoor)
             .then(function (response) {
                 if (response.ok) {
                     response.json().then(function(data) {
+                        // dataProcessor(data);
                         console.log(apiCoor);
                         console.log(data[0]);
                         console.log(data[0].lat);
@@ -37,6 +38,19 @@ var formSubmitHandler = function (event) {
                 }
             })
     };
+    getCoor();
+
+    var getWeather = function() {
+        var lat ='37.5385087'
+        var lon = '-77.43428'
+        apiUrl = apiStart + lat + lonKey + lon + apiKey;
+        fetch(apiUrl)
+            .then(function (response) {
+                console.log(apiUrl);
+                // console.log(apiUrl);
+                return response.json();
+            })
+    }
     getWeather();
     // console.log(lat);
     // console.log(lon);
@@ -46,6 +60,21 @@ var formSubmitHandler = function (event) {
     // getWeather();
     form.reset();
 };
+// function dataProcessor(data){
+//     console.log(data);
+//     var lat = data[0].lat;
+//     var lon = data[0].lon;
+//     console.log(lat);
+//     console.log(lon);
+//     var apiUrl = apiStart + lat + lonKey + lon + apiKey;
+//     fetch(apiUrl)
+//         .then(function (response) {
+//             if (response.ok) {
+//                 response.json().then(function(data) {
+//                 console.log(apiUrl);
+//             });
+//             }
+// })}; 
 
 
 // var getCityCoordinates = function () {
